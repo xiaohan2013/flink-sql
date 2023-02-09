d2ab88091b367b15f2af4bd3a5909126307161ee
xiaohan2013 <xiaohan364@yahoo.com>
2023-02-09 15:47:36 +0800

    stream and sql/table sink

--- src/main/java/com/xiaozhu/data/idmapping/PreciseDistinct.java
+++ src/main/java/com/xiaozhu/data/idmapping/PreciseDistinct.java
@@ -1,26 +1,22 @@
 package com.xiaozhu.data.idmapping;
 
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 import org.apache.flink.table.functions.AggregateFunction;
 import org.roaringbitmap.longlong.Roaring64NavigableMap;
 
 //按照维度来计算
-class PreciseAccumulator{
-    private Roaring64NavigableMap bitmap;
-
-    public PreciseAccumulator(){
-        bitmap=new Roaring64NavigableMap();
-    }
-
-    public void add(long id){
-        bitmap.addLong(id);
-    }
-
-    public long getCardinality(){
-        return bitmap.getLongCardinality();
-    }
-}
-
 public class PreciseDistinct extends AggregateFunction<Long, PreciseAccumulator> {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        StreamTableEnvironment streamTableEnvironment = StreamTableEnvironment.create(env);
+
+        // streamTableEnvironment.createFunction("PreciseDistinct", PreciseDistinct.class);
+        streamTableEnvironment.createTemporarySystemFunction("PreciseDistinct", PreciseDistinct.class);
+        streamTableEnvironment.executeSql("select PreciseDistinct(order_id), price from (VALUES (1, 2.0), (2, 3.1))  AS t(order_id, price)").print();
+
+    }
 
     @Override
     public PreciseAccumulator createAccumulator() {
